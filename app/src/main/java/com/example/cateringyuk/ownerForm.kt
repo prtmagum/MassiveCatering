6f7c3bfffd130a0cae796433d62286a59a904c1d
rizkosefta <seftaal89@gmail.com>
2023-12-13 20:00:36 +0700

    owner

---
+++ app/src/main/java/com/example/cateringyuk/ownerForm.kt
@@ -0,0 +1,11 @@
+package com.example.cateringyuk
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ownerForm : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_owner_form)
+    }
+}
